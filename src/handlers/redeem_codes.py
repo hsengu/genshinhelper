44b0945413c910d352b9bd4f21cff0ca92f86ec1
Hok S. Uy <hoksuy@gmail.com>
2024-11-08 22:21:46 -0800

    Exception handling for expired codes

--- src/handlers/redeem_codes.py
+++ src/handlers/redeem_codes.py
@@ -121,11 +121,11 @@ class RedeemCodes(commands.Cog):
             except genshin.errors.GenshinException as e:
                 if e.retcode == -2001:
                     embed.description = f"Code {code} has expired."
-                if e.retcode == -2003:
+                elif e.retcode == -2003:
                     embed.description = f"Code {code} is invalid."
                 else:
                     logger.exception(f"Code can't be claimed: {e.retcode}")
                     raise e
 
             await ctx.edit(embeds=embeds)
-            await asyncio.sleep(10)
+            await asyncio.sleep(7)
