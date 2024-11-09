ce9d2863ff5ce91a5496144899381a777aaa3f96
Hok S. Uy <hoksuy@gmail.com>
2024-11-08 21:58:19 -0800

    Exception handling for expired codes

--- src/handlers/redeem_codes.py
+++ src/handlers/redeem_codes.py
@@ -110,7 +110,7 @@ class RedeemCodes(commands.Cog):
                     except genshin.errors.GenshinException as e:
                         if e.retcode == -2001:
                             embed.description = f"Code {code} is expired."
-                        if e.retcode == -2017:
+                        elif e.retcode == -2017:
                             already_claimed += 1
                         else:
                             raise e
@@ -123,7 +123,7 @@ class RedeemCodes(commands.Cog):
             except genshin.errors.GenshinException as e:
                 if e.retcode == -2001:
                     embed.description = f"Code {code} is expired."
-                if e.retcode == -2003:
+                elif e.retcode == -2003:
                     embed.description = f"Code {code} is invalid."
                 else:
                     logger.exception(f"Code can't be claimed: {e.retcode}")
