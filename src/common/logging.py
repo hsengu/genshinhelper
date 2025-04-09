d0cfe5c69603aab52887ade7c405097fcf51d4ea
Hok S. Uy <hoksuy@gmail.com>
2025-04-08 21:44:00 -0700

    Adding logging time

--- src/common/logging.py
+++ src/common/logging.py
@@ -1,7 +1,10 @@
 import logging
 
 # Configure logging module. If not set, pycord will not show any logging.
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 # Provide a logger for our own application
 logger = logging.getLogger()
