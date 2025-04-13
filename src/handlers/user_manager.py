3bc592f7edf4b4ebfa9cc8b09d8e9e1ce05463ce
Hok S. Uy <hoksuy@gmail.com>
2025-04-13 11:08:42 -0700

    Consolidating code redemption

--- src/handlers/user_manager.py
+++ src/handlers/user_manager.py
@@ -308,18 +308,6 @@ ALL_PREFERENCES = [
         value=Preferences.AUTO_REDEEM,
         guild_level=3,
     ),
-    #     PreferenceOption(
-    #     label="Auto code redemption for Honkai Star Rail",
-    #     description="New codes are auto-redeemed when found",
-    #     value=Preferences.HSR_AUTO_REDEEM,
-    #     guild_level=3,
-    # ),
-    #     PreferenceOption(
-    #     label="Auto code redemption for Zenless Zone Zero",
-    #     description="New codes are auto-redeemed when found",
-    #     value=Preferences.ZZZ_AUTO_REDEEM,
-    #     guild_level=3,
-    # ),
 ]
 
 
