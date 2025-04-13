2042ff654f9ca12a83e34f0da04ea18ca3f9d615
Hok S. Uy <hoksuy@gmail.com>
2025-04-13 11:32:02 -0700

    Consolidating code redemption

--- src/handlers/redeem_codes.py
+++ src/handlers/redeem_codes.py
@@ -26,8 +26,8 @@ class RedeemCodes(commands.Cog):
     async def redeem(
         self,
         ctx: ApplicationContext,
-        codes: Option(str, "Codes separated by commas"),
         game: Option(str, "Game to redeem for (pick 1, default is genshin): genshin,hsr,zzz", name="game", default="genshin"),
+        codes: Option(str, "Codes separated by commas"),
         target: Option(str, "UID or 'all' for everyone", name="for", default=False),
     ):
         target: str = target or "all"
