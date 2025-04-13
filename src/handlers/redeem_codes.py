e52492529c964601d05fb466fd7154170efb3268
Hok S. Uy <hoksuy@gmail.com>
2025-04-13 13:41:20 -0700

    Consolidating code redemption

--- src/handlers/redeem_codes.py
+++ src/handlers/redeem_codes.py
@@ -129,7 +129,7 @@ class RedeemCodes(commands.Cog):
                         else:
                             logger.exception(f"\t\t{ctx.author.id} code can't be claimed: {e.retcode}")
                             # raise e
-                        logger.exception(f"!!! {e}")
+                        logger.exception(f"!!! {dir(e)}")
 
                 embed.description = f"Redeemed {game} code {code} for {redeemed} accounts."
                 if already_claimed:
