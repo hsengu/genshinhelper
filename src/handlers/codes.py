42568d692fe7ce3fb61e303d5eabdc44d6da005c
Hok S. Uy <hoksuy@gmail.com>
2025-06-20 14:53:57 -0700

    Added stoken field to register for automatic cookie renewals

--- src/handlers/codes.py
+++ src/handlers/codes.py
@@ -110,6 +110,7 @@ class CodeScanner(commands.Cog):
             for account in accounts:
                 if not account.settings[Preferences.AUTO_REDEEM]:
                     continue
+                account.validate()
                 logger.info(f"\t {game_name} >>> Redeeming code {code} for account {account.mihoyo_id}")
                 queue.append(asyncio.create_task(account.client.redeem_code(code=code, game=redeem_game)))
 
